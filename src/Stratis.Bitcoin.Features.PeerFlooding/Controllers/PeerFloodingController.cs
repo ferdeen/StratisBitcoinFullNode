c497fb9dfbd2768866c161604b52413f43656291
Ferdeen Mughal <Ferdeen.Mughal@StratisPlatform.com>
2018-11-02 17:05:06 +0000

    Initial SendTransactionsWithLowFee() endpoint work.

--- src/Stratis.Bitcoin.Features.PeerFlooding/Controllers/PeerFloodingController.cs
+++ src/Stratis.Bitcoin.Features.PeerFlooding/Controllers/PeerFloodingController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Flurl;
 using Flurl.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin.Features.Wallet;
 using Stratis.Bitcoin.Features.Wallet.Models;
 
 namespace Stratis.Bitcoin.Features.PeerFlooding.Controllers
@@ -15,26 +19,13 @@ namespace Stratis.Bitcoin.Features.PeerFlooding.Controllers
     {
         private readonly int totalruns;
         private readonly IEnumerable<KeyValuePair<string, InternalNetworkNodeWallet.NodeWallet>> nodeWallets;
+        private readonly List<string> transactionHexList;
 
         public PeerFloodingController(int totalruns = 10000)
         {
             this.totalruns = totalruns;
             this.nodeWallets = InternalNetworkNodeWallet.Map.Where(nw => nw.Value.HasFunds == true).Take(10);
-        }
-
-        [Route("OverrideMempoolSettings")]
-        [HttpPost]
-        public void OverrideMempoolSettings()
-        {
-            var count = this.nodeWallets.Count();
-
-            /* TODO - Pass params to override mempool contants
-            MempoolBehaviour    InventoryBroadcastMax
-            MempoolValidator    DefaultAncestorLimit
-            MempoolValidator    DefaultAncestorSizeLimit
-            MempoolValidator    DefaultDescendantLimit
-            MempoolValidator    DefaultDescendantSizeLimit
-            */
+            this.transactionHexList = new List<string>();
         }
 
         [Route("RecoverWallets")]
@@ -56,21 +47,59 @@ namespace Stratis.Bitcoin.Features.PeerFlooding.Controllers
             });
         }
 
-        /* TODO - create next api endpoints.
-
-        [Route("SendTransactionWithFee")]
+        [Route("SendTransactionsWithLowFee")]
         [HttpGet]
-        public void SendTransactionWithLowFee()
+        public void SendTransactionsWithLowFee()
         {
-            // for each wallet
+            Parallel.ForEach(this.nodeWallets, async (nodeWallet) =>
+            {
+                string nodeWalletKey = nodeWallet.Key;
 
-            // send 1,000 txn (and save hex values)
-            // every 5 seconds
+                var unusedAddress = await "http://localhost:37221/api/wallet/unusedAddress"
+                    .SetQueryParams(new { walletName = InternalNetworkNodeWallet.Map[nodeWalletKey].WalletName, accountName = "account 0" })
+                    .GetJsonAsync<string>();
 
+                var request = new BuildTransactionRequest
+                {
+                    WalletName = InternalNetworkNodeWallet.Map[nodeWalletKey].WalletName,
+                    AccountName = "account 0",
+                    AllowUnconfirmed = true,
+                    Amount = "1",
+                    FeeType = "low",
+                    Password = InternalNetworkNodeWallet.Map[nodeWalletKey].Password,
+                    DestinationAddress = unusedAddress.ToString()
+                };
 
-            // return true
+                for (int i = 0; i <= 1000; i++)
+                {
+                    var newTransaction = await "http://localhost:37221/api/wallet/build-transaction"
+                        .PostJsonAsync(request)
+                        .ReceiveJson();
+
+                    string hex = (newTransaction as ExpandoObject).FirstOrDefault(x => x.Key == "hex").Value.ToString();
+
+                    // Save transaction hex, for flooding the network after the network has been reset.
+                    this.transactionHexList.Add(hex);
+
+                    try
+                    {
+                        var response = await "http://localhost:37221/api/wallet/send-transaction"
+                            .PostJsonAsync(new SendTransactionRequest(hex));
+
+                        Thread.Sleep(5000);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.Message.Contains("code 400"))
+                        {
+                            Thread.Sleep(60000);
+                        }
+                    }
+                }
+            });
         }
 
+        /*
         [Route("FloodNetwork")]
         [HttpGet]
         public void FloodNetwork()
@@ -84,7 +113,6 @@ namespace Stratis.Bitcoin.Features.PeerFlooding.Controllers
 
             // check NodeEA this node (has been banned).
         }
-
         */
     }
 }
